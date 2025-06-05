d22d3e6fbf6478c64641e4e2e37d705d64526290
Shyamraj V S <shyamrajvs737@gmail.com>
2025-06-05 19:51:19 +0530

    profile done

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -7,7 +7,7 @@ export const getUsersForSidebar = async (req, res) => {
     const filteredUsers = await User.find({
       _id: { $ne: loggedInUser },
     }).select("-password");
-    res.status(200).jsonment(filteredUsers);
+    res.status(200).json(filteredUsers);
   } catch (error) {
     console.log("Error in getUsersForSidebar controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,9 @@ import messageRoutes from "./routes/message.route.js";
 const app = express();
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
 app.use(cookieParser());
 app.use(
   cors({
